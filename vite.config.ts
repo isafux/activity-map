7e757307825a6d98ee724520757fbe75d082c754
Isa <isafu@gmx.at>
2025-11-12 16:47:01 +0100

    add base in config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: '/activity-map/',
   css: {
     preprocessorOptions: {
       scss: {
